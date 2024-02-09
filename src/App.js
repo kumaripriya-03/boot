b0a4f37b338591195f518a76a7365f59a450c1a4
kumaripriya-03 <159520814+kumaripriya-03@users.noreply.github.com>
2024-02-09 17:32:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import UseState from './components/Hooks/useState'
+// import UseEffect from './components/Hooks/useEffect'
+import UseReducer from './components/Hooks/useReducer'
+
+ const App = () => {
+  return (
+    <>
+      {/* <UseState/> */}
+      {/* <UseEffect/> */}
+      <UseReducer/>
+    </>
+  )
+}
+
+export default App
